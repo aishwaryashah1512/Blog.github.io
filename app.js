c0e0415c56643a7295098dd233c036f1410008cc
unknown <aishwaryashah1235@gmail.com>
2020-08-19 01:08:07 -0700

    final

--- app.js
+++ app.js
@@ -77,7 +77,13 @@ app.get("/compose",function(req,res)
 res.render("compose");
 });
 
-app.listen(3000,function()
+let port=process.env.PORT;
+if(port==null||port="")
 {
-  console.log("Server started on port 3000");
+  port=3000;
+}
+
+app.listen(port,function()
+{
+  console.log("Server started on port ");
 });
